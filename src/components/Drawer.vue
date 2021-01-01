2323ada7a783e411b57a6f69cd5affefd6e575a3
GiantZwlin <giantzwlin@gmail.com>
2021-01-01 21:21:08 +0800

    init Project

---
+++ src/components/Drawer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-navigation-drawer app clipped>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "Drawer",
+  data() {
+    return {
+      items: [
+        {title: 'Dashboard', icon: 'mdi-view-dashboard'},
+        {title: 'Photos', icon: 'mdi-image'},
+        {title: 'About', icon: 'mdi-help-box'},
+      ],
+      right: null,
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar
+      app
+      flat
+      clipped-left
+      outlined
+      color="gray"
+  >
+    <v-btn icon>
+      <v-icon>mdi-clipboard-outline</v-icon>
+    </v-btn>
+    <v-toolbar-title>QuickShare</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-information-outline</v-icon>
+    </v-btn>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
